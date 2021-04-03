0e11d23c591fc3cb0d4e0d1007ef43be653526cd
jvmistica <zombossjoey@tutanota.com>
2021-04-04 00:17:58 +0800

    Correct backups folder

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 # List all the projects to be backed-up and create a TAR file
 # for each of them in the backups folder
-
 projects=(`ls /path/to/projects/`)
 for project in ${projects[@]}
 do
-	tar -czvf /path/to/projects/${project}_$(date +%Y%m%d%H%M%S).tar.gz -C /path/to/projects/ ${project}
+	tar -czvf /path/to/backups/${project}_$(date +%Y%m%d%H%M%S).tar.gz -C /path/to/projects/ ${project}
 done
 
