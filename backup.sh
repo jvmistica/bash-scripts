2bebb9e211b8e02dbcf3ec364115e94a97d1258f
jvmistica <zombossjoey@tutanota.com>
2021-04-03 23:56:05 +0800

    Add iteration and backup of individual projects

--- backup.sh
+++ backup.sh
@@ -1,2 +1,10 @@
-# Creates a TAR file of a project in the backups folder
-tar -czvf /home/<user>/backups/projects/<project>_$(date +%Y%m%d%H%M%S).tar.gz -C /home/<user>/projects/shell/ <project>
+#!/bin/bash
+# List all the projects to be backed-up and create a TAR file
+# for each of them in the backups folder
+
+projects=(`ls /path/to/projects/`)
+for project in ${projects[@]}
+do
+	tar -czvf /path/to/projects/${project}_$(date +%Y%m%d%H%M%S).tar.gz -C /path/to/projects/ ${project}
+done
+
